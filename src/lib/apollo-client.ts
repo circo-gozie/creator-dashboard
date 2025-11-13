22c42b8cb2f436663345ac8032cbf752e0e102e1
Goz1e <phenominal55@gmail.com>
2025-11-13 08:04:59 +0100

    fix: cookie error

--- src/lib/apollo-client.ts
+++ src/lib/apollo-client.ts
@@ -1,25 +1,25 @@
 import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 import { SetContextLink } from "@apollo/client/link/context";
-import Cookies from 'js-cookie';
+import { getCookie } from "cookies-next";
 
 const httpLink = new HttpLink({
-    uri: process.env.NEXT_PUBLIC_API_URL,
-    credentials: 'include',
+  uri: process.env.NEXT_PUBLIC_API_URL,
+  credentials: "include",
 });
 
 const authLink = new SetContextLink((prevContext) => {
-    const accessToken = Cookies.get('accessToken');
-    return {
-        headers: {
-            ...prevContext.headers,
-            Authorization: accessToken ? `Bearer ${accessToken}` : '',
-        },
-    };
+  const accessToken = getCookie("accessToken");
+  return {
+    headers: {
+      ...prevContext.headers,
+      Authorization: accessToken ? `Bearer ${accessToken}` : "",
+    },
+  };
 });
 
 const client = new ApolloClient({
-    link: authLink.concat(httpLink),
-    cache: new InMemoryCache(),
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
 });
 
 export default client;
